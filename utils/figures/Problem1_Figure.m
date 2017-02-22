b98ea80dab58a3975ce5a2c4801f2522d0b46430
nad <nbfigueroa@gmail.com>
2017-02-22 13:25:58 +0100

    Created figures for Intro

--- utils/figures/Problem1_Figure.m
+++ utils/figures/Problem1_Figure.m
@@ -3,14 +3,28 @@ clear all; close all; clc;
 load('probl1_gmm.mat')
 
 %% Visualize Covariance Dataset
-title_name  = 'INPUT: Dataset $\Theta$ of Covariance Matrices';
+title_name  = 'Dataset $\Theta$ of Covariance Matrices';
 plot_labels = {'$x_1$','$x_2$'};
-font_size   = 20;
+font_size   = 18;
 
 figure('Color',[1 1 1])
-
-colors     = hsv(5);
-
+colored = 1;
+% Clustered Colors
+if colored
+    vivid_c = hsv(3);
+    % Thin ellipse (diag covariance)
+    colors(1,:) = vivid_c(1,:);
+    colors(6,:) = vivid_c(1,:);
+    % Circle (isometric covariance)
+    colors(2,:) = vivid_c(2,:);
+    colors(3,:) = vivid_c(2,:);
+    % Fat ellipse (Full covariance)
+    colors(4,:) = vivid_c(3,:);
+    colors(5,:) = vivid_c(3,:);
+else
+    % Gray Color
+    colors = repmat([0.3    0.3    0.3],[6,1]);
+end
 for i=1:length(gmm.Priors)
     hold on
     plotGMM(gmm.Mu(:,i), gmm.Sigma(:,:,i), colors(i,:),1);   
@@ -24,6 +38,7 @@ xlabel(plot_labels{1},'Interpreter','Latex', 'FontSize',font_size,'FontName','Ti
 ylabel(plot_labels{2},'Interpreter','Latex','FontSize',font_size,'FontName','Times', 'FontWeight','Light');
 % legend({'$\theta_1$','$\theta_2$','$\theta_3$','$\theta_4$','$\theta_5$'},'Interpreter','Latex','FontSize',font_size)
 title (title_name,'Interpreter','Latex','FontSize',font_size,'FontName','Times', 'FontWeight','Light');          
+axis equal
 grid on; box on;
 
 
