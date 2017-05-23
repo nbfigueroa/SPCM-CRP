bd40b1689a4a764a57d726a5ff8bd1b357caa783
nad <nbfigueroa@gmail.com>
2017-05-23 11:03:51 +0200

    Script and results from hyper-parameter sensitivity tests

--- run_hyperparameter_tests.m
+++ run_hyperparameter_tests.m
@@ -77,120 +77,101 @@ dataset_name = 'Synthetic DT-MRI';
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % %%%%%%%%%%%%%%%%%%%%% Set Hyper-parameter %%%%%%%%%%%%%%%%%%%%%%%%
-% Tolerance for SPCM decay function 
-tau = 1; % [1, 100] Set higher for noisy data, Set 1 for ideal data 
-% Datasets 1-3:  tau = 1;
-% Datasets 4a/4b tau = 5;
-% Datasets 4a/4b tau = 5;
-% Dataset 6: tau = 1;
+steps = 20;
+Sampler_Stats = [];
+tau_range   = logspace(log10(0.1),log10(50),steps);
+alpha_range = logspace(log10(0.1),log10(20),steps);
 
 % %%%%%% Compute Confusion Matrix of Similarities %%%%%%%%%%%%%%%%%%
-spcm = ComputeSPCMfunctionMatrix(sigmas, tau);  
-S = spcm(:,:,2);
-
-%%%%%%% Visualize Bounded Similarity Confusion Matrix %%%%%%%%%%%%%%
-if exist('h0','var') && isvalid(h0), delete(h0); end
-title_str = 'Bounded Similarity Function (B-SPCM) Matrix';
-h0 = plotSimilarityConfMatrix(S, title_str);
-
-%%%%%%%%%%% Automatic Discovery of Dimensionality on M Manifold %%%%%%%%%%%
-M = [];
-[Y, d, thres, V] = spectral_DimRed(S, M);
-if isempty(M)
-    s_norm = normalize_soft(softmax(d));
-    M = sum(s_norm <= thres);
-end
-
-%%%%%%%% Visualize Spectral Manifold Representation for M=2 or M=3 %%%%%%%%
-if exist('h1','var') && isvalid(h1), delete(h1);end
-h1 = plotSpectralManifold(Y, true_labels, d,thres, s_norm, M);
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%                        Run sampler T times                            %%
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-T = 20;
-Sampler_Stats = [];
-for run=1:T    
+for i_tau=1:steps
+    tau = tau_range(i_tau)
+    spcm = ComputeSPCMfunctionMatrix(sigmas, tau);
+    S = spcm(:,:,2);
     
-    %%%%%%%% Non-parametric Clustering on Manifold Data with Sim prior %%%%%%%%
-    % Setting sampler/model options (i.e. hyper-parameters, alpha, Covariance matrix)
-    options                 = [];
-    options.type            = 'full';  % Type of Covariance Matrix: 'full' = NIW or 'Diag' = NIG
-    options.T               = 500;     % Sampler Iterations
-    options.alpha           = 1;     % Concentration parameter
+    %%%%%%% Visualize Bounded Similarity Confusion Matrix %%%%%%%%%%%%%%
+    if exist('h0','var') && isvalid(h0), delete(h0); end
+    title_str = 'Bounded Similarity Function (B-SPCM) Matrix';
+    h0 = plotSimilarityConfMatrix(S, title_str);
     
-    % Standard Base Distribution Hyper-parameter setting
-    if strcmp(options.type,'diag')
-        lambda.alpha_0       = M;                    % G(sigma_k^-1|alpha_0,beta_0): (degrees of freedom)
-        lambda.beta_0        = sum(diag(cov(Y')))/M; % G(sigma_k^-1|alpha_0,beta_0): (precision)
+    %%%%%%%%%%% Automatic Discovery of Dimensionality on M Manifold %%%%%%%%%%%
+    M = [];
+    [Y, d, thres, V] = spectral_DimRed(S, M);
+    if isempty(M)
+        s_norm = normalize_soft(softmax(d));
+        M = sum(s_norm <= thres);
     end
-    if strcmp(options.type,'full')
-        lambda.nu_0        = M;                           % IW(Sigma_k|Lambda_0,nu_0): (degrees of freedom)
-        lambda.Lambda_0    = eye(M)*sum(diag(cov(Y')))/M; % IW(Sigma_k|Lambda_0,nu_0): (Scale matrix)
+    
+    %%%%%%%% Visualize Spectral Manifold Representation for M=2 or M=3 %%%%%%%%
+    if exist('h1','var') && isvalid(h1), delete(h1);end
+    h1 = plotSpectralManifold(Y, true_labels, d,thres, s_norm, M);
+    
+    for iter=1:steps
+        alpha = alpha_range(iter)
+        %%%%%%%% Non-parametric Clustering on Manifold Data with Sim prior %%%%%%%%
+        % Setting sampler/model options (i.e. hyper-parameters, alpha, Covariance matrix)
+        options                 = [];
+        options.type            = 'full';             % Type of Covariance Matrix: 'full' = NIW or 'Diag' = NIG
+        options.T               = 200;                % Sampler Iterations
+        options.alpha           = alpha;  % Concentration parameter
+        
+        % Standard Base Distribution Hyper-parameter setting
+        if strcmp(options.type,'diag')
+            lambda.alpha_0       = M;                    % G(sigma_k^-1|alpha_0,beta_0): (degrees of freedom)
+            lambda.beta_0        = sum(diag(cov(Y')))/M; % G(sigma_k^-1|alpha_0,beta_0): (precision)
+        end
+        if strcmp(options.type,'full')
+            lambda.nu_0        = M;                           % IW(Sigma_k|Lambda_0,nu_0): (degrees of freedom)
+            lambda.Lambda_0    = eye(M)*sum(diag(cov(Y')))/M; % IW(Sigma_k|Lambda_0,nu_0): (Scale matrix)
+%             lambda.Lambda_0    = diag(diag(cov(Y')));         % IW(Sigma_k|Lambda_0,nu_0): (Scale matrix)
+        end
+        lambda.mu_0             = mean(Y,2);    % hyper for N(mu_k|mu_0,kappa_0)
+        %     lambda.mu_0             = zeros(size(Y(:,1)));    % hyper for N(mu_k|mu_0,kappa_0)
+        lambda.kappa_0          = 1;            % hyper for N(mu_k|mu_0,kappa_0)
+        
+        
+        % Run Collapsed Gibbs Sampler
+        options.lambda    = lambda;
+        [Psi Psi_Stats]   = run_ddCRP_sampler(Y, S, options);
+        est_labels        = Psi.Z_C';
+        
+        
+        Sampler_Stats(iter,i_tau).Psi = Psi;
+        Sampler_Stats(iter,i_tau).Psi_Stats = Psi_Stats;
     end
-    lambda.mu_0             = mean(Y,2);    % hyper for N(mu_k|mu_0,kappa_0)
-%     lambda.mu_0             = zeros(size(Y(:,1)));    % hyper for N(mu_k|mu_0,kappa_0)
-    lambda.kappa_0          = 1;            % hyper for N(mu_k|mu_0,kappa_0)
-    
-    
-    % Run Collapsed Gibbs Sampler
-    options.lambda    = lambda;
-    [Psi Psi_Stats]   = run_ddCRP_sampler(Y, S, options);
-    est_labels        = Psi.Z_C';
-    
-    
-    Sampler_Stats(run).Psi = Psi;
-    Sampler_Stats(run).Psi_Stats = Psi_Stats;
 end
-
-%% %%%%%% Visualize Collapsed Gibbs Sampler Convergence %%%%%%%%%%%%%%
-figure('Color',[1 1 1])
-Iterations    = length(Sampler_Stats(1).Psi_Stats.LogLiks);
-T = length(Sampler_Stats)
-cluster_ests  = zeros(1,T);
-for i=1:T    
-    Psi_Stats = Sampler_Stats(i).Psi_Stats;
-    Psi = Sampler_Stats(i).Psi;
-    cluster_ests(i) = Psi_Stats.TotalClust(Psi.Maxiter); 
-%     semilogx(1:length(Psi_Stats.PostLogProbs),Psi_Stats.PostLogProbs,'--*', 'LineWidth',2,'Color',[rand rand rand]); hold on;
-    plot(1:length(Psi_Stats.PostLogProbs),Psi_Stats.PostLogProbs,'--*', 'LineWidth',2,'Color',[rand rand rand]); hold on;
-end
-xlim([1 Iterations])
-xlabel('Gibbs Iteration','Interpreter','LaTex','Fontsize',20); ylabel('LogPr','Interpreter','LaTex','Fontsize',20)
-title ({sprintf('Trace of Posterior Probabilities $p(C|Y,S, \\alpha, \\lambda)$ for %d runs, $\\overline{K}=$%2.1f (%2.1f)',[T mean(cluster_ests) std(cluster_ests)])}, 'Interpreter','LaTex','Fontsize',20)
-grid on
-
 
 %% %%%%%% Compute Collapsed Gibbs Sampler Clustering vs Ground Truth %%%%%%%%%%%%%%
-cluster_purity = zeros(T,Iterations);
-cluster_NMI    = zeros(T,Iterations);
-cluster_F      = zeros(T,Iterations);
-for i=1:T    
-    Psi_Stats = Sampler_Stats(i).Psi_Stats;
-    for j=1:Iterations
-     est_labels = Psi_Stats.TableAssign(:,j);   
-     [cluster_purity(i,j) cluster_NMI(i,j) cluster_F(i,j)] = cluster_metrics(true_labels, est_labels');    
+cluster_purity = zeros(steps,steps);
+cluster_NMI    = zeros(steps,steps);
+cluster_F      = zeros(steps,steps);
+for i_tau=1:steps
+    for iter=1:steps
+    Psi       = Sampler_Stats(iter,i_tau).Psi;
+    Psi_Stats = Sampler_Stats(iter,i_tau).Psi_Stats;
+    est_labels = Psi.Z_C;
+    [cluster_purity(iter,i_tau) cluster_NMI(iter,i_tau) cluster_F(iter,i_tau)] = cluster_metrics(true_labels, est_labels');    
     end
 end
 
 %% %%%%%% Visualize Collapsed Gibbs Sampler Clustering vs Ground Truth %%%%%%%%%%%%%%
-figure('Color',[1 1 1])
-for i=1:T    
-semilogx(1:length(cluster_purity),cluster_F(i,:),'--*', 'LineWidth',2,'Color',[rand rand rand]); hold on;
-end
-xlabel('Gibbs Iteration','Interpreter','LaTex','Fontsize',20); ylabel('$\mathcal{F}$-Measure','Interpreter','LaTex','Fontsize',20)
-title ({sprintf('Estimated Clusters vs. Ground Truth over %d runs',[T])}, 'Interpreter','LaTex','Fontsize',20)
-xlim([1 Iterations])
-grid on
 
-%% %%%%%% Visualize Collapsed Gibbs Sampler Computation Time %%%%%%%%%%%%%%
-figure('Color',[1 1 1])
-overall_comp = zeros(1,T);
-for i=1:T    
-    Psi_Stats = Sampler_Stats(i).Psi_Stats;
-    overall_comp(i) = sum(Psi_Stats.CompTimes);
-    semilogx(1:length(Psi_Stats.CompTimes),Psi_Stats.CompTimes,'--*', 'LineWidth',2','Color',[rand rand rand]); hold on; 
-end
-xlim([1 Iterations])
-xlabel('Gibbs Iteration','Interpreter','LaTex','Fontsize',20); ylabel('Computation Time (s)','Interpreter','LaTex','Fontsize',20);
-title ({sprintf('Computation times per/iteration for %d runs',[T])}, 'Interpreter','LaTex','Fontsize',20)
-grid on
+figure('Color',[1 1 1]);
+colormap hot;
+x = tau_range;
+y = alpha_range;
+z = cluster_F;
+contourf(x,y,z)
+
+set(gca,'xscale','log')
+set(gca,'yscale','log')
+set(gca, 'XTick', tau_range)
+set(gca,'XTickLabel', cellstr(num2str(tau_range(:), '%4.2f')))
+set(gca, 'YTick', alpha_range)
+set(gca,'YTickLabel', cellstr(num2str(alpha_range(:), '%4.2f')))
+
+title({'$\mathcal{F}$-Measure'},'Interpreter','LaTex','FontSize',20)
+ylabel('Concentration Parameter ($\alpha$)','Interpreter','LaTex','FontSize',20);
+xlabel('Tolerance Parameter ($\tau$)','Interpreter','LaTex','FontSize',20);
+colorbar
+grid off
+axis square
